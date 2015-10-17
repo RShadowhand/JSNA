3510376bc7e92e876551fd89843bfdc7c8ffc135
Shadowhand <r.shadowhand@gmail.com>
2015-10-17 23:38:29 +0300

    forgot to add files!

---
+++ templates/ObjectTemplate.js
@@ -0,0 +1,29 @@
+function Obj(args) {
+	var g = this; // Needed for scoping.
+
+	// Write your initalizing code here.
+	// They'll be ran when you create an instance of this object.
+	g.init = function(args){
+
+	}
+
+	// Load your content for this object here.
+	// Call this from game's loadContent()
+	g.loadContent = function(){
+
+	}
+
+	// Call this from your game update().
+	// Pass Keyboard from the game if you want to let this object control itself.
+	g.update = function (Keyboard) {
+		
+	}
+	
+	// Call this from your game draw().
+	// It can draw itself if you pass canvas to it.
+	g.draw = function (canvas) {
+
+	}
+
+	this.init(args); // Initialize the object.
+}
---
+++ templates/game.js
@@ -0,0 +1,60 @@
+function game(){
+	var g = this; // Needed for scoping.
+
+	g.init = function(){
+		g.k = new Keyboard();
+		g.recanvas(); // Fix the draw space.
+		g.loadContent(); // Load the content for your objects.
+
+		{ /* Your init code here. */
+
+		}
+
+		var DESIRED_FRAMES = 60; // How many FPS do we want?
+		var RENDER_TIME = 1000 / DESIRED_FRAMES; // Calculate the time it would take to render.
+	    g.loop = setInterval(g.update, RENDER_TIME); // Start the game logic.
+	}
+
+	g.recanvas = function(){
+		surface = document.getElementById("canvas"); // Get Canvas.
+		canvas = surface.getContext("2d"); // Get Context (2D).
+		surface.width = window.innerWidth; // Set width.
+	   	surface.height = window.innerHeight; // Set height.
+	}
+
+	// Declare your variables as empty outside loadContent(), 
+	// then finalize them in loadContent. This way it'll be in the game's scope.
+	g.loadContent = function (argument) {
+		
+	}
+	
+	g.update = function(){
+		{ /* Game logic */ 
+
+		}
+
+		{ /* Prepare the screen. */
+			g.prepareScreen(); 	
+		}
+	}
+
+	g.prepareScreen = function() {
+		{ /* Clear the screen first */ 
+			canvas.fillStyle = "black";
+			canvas.fillRect(0,0, surface.width, surface.height);
+		}
+
+		{ /* Then send to drawing. */ 
+			g.draw();
+		}
+	}
+
+	// Fill the screen!
+	g.draw = function() {
+		
+	}
+
+	this.init(); // Initialize the game.
+}
+
+var app = new game(); // Create a new instance of game, which also initializes itself immediately.
